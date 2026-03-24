10d7be96b231c0e3b8c64c569c6478c1e7cea06d
RamOkami <1franchute2004@gmail.com>
2026-03-24 15:40:37 -0300

    comienzo de proyecto

--- app.js
+++
@@ -1,8 +0,0 @@
-let boton = document.getElementById('boton');
-let titulo = document.getElementById('titulo-principal');
-
-
-boton.addEventListener('click', function (){
-    titulo.innerHTML = '¡Se ha presionado el boton!';
-    titulo.style.color = 'purple';
-});
---
+++ assets/js/app.js
@@ -0,0 +1,6 @@
+let titulo = document.getElementById('title');
+let hoover = document.getElementById('hover');
+
+hoover.addEventListener('click', function (){
+    titulo.innerHTML = 'HOLA MUNDO';
+});
